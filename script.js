d81e8ade2ae073e6cd6929153f6643f05a580f94
krs <rusters007@gmail.com>
2025-06-08 01:09:02 +0500

    сделал карусель

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var scriptURL = 'https://script.google.com/macros/s/AKfycbxyBDsb5TAkwLa1p79vsB-R
 var form = document.forms['submit-to-google-sheet']
 var successMessage = document.getElementById('success-message');
 var sendingMessage = document.getElementById('sending-message');
-
 openModalBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
 });
@@ -21,6 +20,28 @@ window.addEventListener('click', (e) => {
         modal.style.display = 'none';
     }
 });
+
+//работа с каруселью
+let currentIndex = 0;  
+const carouselItems = document.querySelectorAll('.carousel-item');  
+  
+function goToSlide(index) {  
+    if (index < 0) {  
+        index = carouselItems.length - 1;  
+    } else if (index > carouselItems.length) {  
+        index = 0;  
+    }  
+    currentIndex = index;  
+    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;  
+}  
+function goToNextSlide() {  
+    goToSlide(currentIndex + 1);
+}  
+function goToPrevSlide() {  
+    goToSlide(currentIndex - 1);  
+}  
+setInterval(goToNextSlide, 3000); // автоматическая прокрутка каждые 3 секунды  
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
